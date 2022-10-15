e633ea022b009ac397d19cd971334c4f1f1e4433
MoinHosseini <moeinaldin2022@gmail.com>
2022-10-15 09:52:03 +0330

    added models

--- product/admin.py
+++ product/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from .models import kala,material,measure,calc
+from .models import kala,material,measurement,cart
 
 # Register your models here.
 
 admin.site.register(kala)
 admin.site.register(material)
-admin.site.register(measure)
-admin.site.register(calc)
+admin.site.register(measurement)
+admin.site.register(cart)
