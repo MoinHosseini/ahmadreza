f824569add02c48d5dbfeabeb9d1fedc9e9999a6
MoinHosseini <moeinaldin2022@gmail.com>
2022-10-24 10:32:57 +0330

    new urls added

--- product/urls.py
+++ product/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 urlpatterns = [
     path("all/",views.all,name="all-products"),
-    path("add/<str:type>/",views.add.as_view(),name="add-products")
+    path("add/",views.add.as_view(),name="add-material"),
+    path("addtocart/",views.CartView.as_view() ,name="add-to-cart"),
+    path("check/",views.checkcart,name="check-cart"),
+    path("create/",views.create,name="create"),
+    path("edit/<int:id>/",views.edit),
 ]
