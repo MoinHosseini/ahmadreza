8de57a798dc0877720190a5c6eebe7cf22f18334
MoinHosseini <111416172+MoinHosseini@users.noreply.github.com>
2022-12-10 11:57:41 +0330

    final changes

--- hesaban/settings.py
+++ hesaban/settings.py
@@ -96,7 +96,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'product.context_processors.access_mina'
+                # 'product.context_processors.access_mina',
             ],
         },
     },
