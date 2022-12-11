aad8aa7e229ee34d14d6b7f9c1d9888494f7eca5
MoinHosseini <111416172+MoinHosseini@users.noreply.github.com>
2022-12-11 23:21:38 +0330

    urls named and editted

--- product/urls.py
+++ product/urls.py
@@ -7,28 +7,23 @@ urlpatterns = [
 
     path("all/<str:type>",views.all,name="all"),
 
-    ### the below url is being used for adding new materials to the database
     path("add/",login_required(views.add.as_view()),name="add-material"),
-    
     path("addtocart/",login_required(views.CartView.as_view()) ,name="add-to-cart"),
-
     path("create/",views.create,name="create"),
-
     path("check/",views.checkcart,name="check-cart"),
 
     path("remove/<str:type>/<int:id>",views.remove,name="remove"),
     path("edit/<int:id>/",views.edit),
     path("alter/<int:id>/",views.alter),
 
-    path("report/material",views.report),
-    
-    path("report/kala",views.report2),
+    path("report/material",views.report,name="materialreport"),
+    path("report/kala",views.report2,name="kalareport"),
 
-    path("factor/<str:type>",views.factor),
-    path("addtofactor/",views.factoring),
-    path("checkfactor/",views.checkfactor),
+    path("factor/<str:type>",views.factor,name="factor"),
+    path("addtofactor/",views.factoring,name="addtofactor"),
+    path("checkfactor/",views.checkfactor,name="checkfactor"),
     path("removefcart/<int:id>",views.removefcart),
-    path("view/<int:id>",views.factor_content),
+    path("view/<int:id>",views.factor_content , name="view"),
 
-    path("notif/<str:type>",views.notif),
+    path("notif/<str:type>",views.notif,name="notif"),
 ]
